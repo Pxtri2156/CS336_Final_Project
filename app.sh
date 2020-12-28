e6a3c59e3665ee35d5671e53478ef8e48c50d00d
Son Luu <sonlh.cs.uit@gmail.com>
2020-12-29 02:08:34 +0700

    temp

--- app.sh
+++ app.sh
@@ -1,1 +1,9 @@
-python3 app.py
+pip install flask_ngrok
+pip install flask_cors
+mkdir /content/database
+# our database
+cp /content/drive/MyDrive/Official_OnePiece_images/Perfect_One_Piece/* -d /content/database/
+unzip /content/database/OnePiece_images_1.zip -d /content/database/
+unzip /content/database/OnePiece_images_2.zip -d /content/database/
+unzip /content/database/OnePiece_images_3.zip -d /content/database/
+unzip /content/database/OnePiece_images_4.zip -d /content/database/
